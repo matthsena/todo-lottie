b32d2f409531d8b9b645c738896a4d309526b2d5
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-16 17:05:44 -0300

    implementação básica de lottie

--- App.tsx
+++ App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-// import { StyleSheet } from 'react-native';
+import { View, StyleSheet, useColorScheme } from 'react-native';
 import Theme from './src/Theme';
 import { Container } from './src/components/Container';
 import { Text } from './src/components/Text';
+import LottieView from 'lottie-react-native';
 
 const App = () => {
+  const appearanceMode = useColorScheme();
+
   return (
     <Theme>
       <Container>
-        <Text>Ola</Text>
+        <LottieView
+          source={
+            appearanceMode === 'dark'
+              ? require('./src/lottiefiles/blue-man-dark.json')
+              : require('./src/lottiefiles/blue-man.json')
+          }
+          autoPlay
+          loop
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>ToDo Lottie</Text>
+          <Text style={styles.subtitle}>App desenvolvido com Lottie</Text>
+        </View>
       </Container>
     </Theme>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#fff',
-//   },
-// });
+const styles = StyleSheet.create({
+  textContainer: {
+    alignSelf: 'flex-start',
+    flexDirection: 'column',
+  },
+  title: {
+    marginTop: 64,
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  subtitle: {
+    marginVertical: 16,
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
 
 export default App;
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components/native';
 import { ThemeProviderType } from '../Theme';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
   align-items: center;
   justify-content: center;
+  flex-direction: row;
   background-color: ${({ theme }: { theme: ThemeProviderType }) =>
     theme.colors.background};
 `;
