a8f3daf6a76434b6e6d7a74861d5f9ae0e2711de
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-16 23:10:38 -0300

    animação para caso não haja tarefas

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {
+  StatusBar,
+  useColorScheme,
+  StyleSheet,
+  View,
+  Dimensions,
+} from 'react-native';
 import { Container } from '../components/Container';
 import { Text } from '../components/Text';
+import LottieView from 'lottie-react-native';
+
+const { width } = Dimensions.get('window');
+
 const Home = () => {
+  const appearanceMode = useColorScheme();
+
   return (
     <Container>
       <StatusBar barStyle={'light-content'} />
 
-      <Text>Ola mundo</Text>
+      <View style={styles.notFountContent}>
+        <LottieView
+          source={
+            appearanceMode === 'dark'
+              ? require('../lottiefiles/empty-file-dark.json')
+              : require('../lottiefiles/empty-file.json')
+          }
+          autoPlay
+          loop
+        />
+        <Text style={styles.notFoundText}>Sem tarefas por enquanto...</Text>
+      </View>
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  notFountContent: {
+    width: width * 0.8,
+    height: width * 0.8,
+  },
+  notFoundText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    opacity: 0.65,
+  },
+});
 
 export default Home;
